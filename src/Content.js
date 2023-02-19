adea37234f24c7254ad2a1fafc390dff37d65d1b
Kishan Kulkarni <98462020+Kishan-Kulkarni@users.noreply.github.com>
2023-02-19 20:06:21 +0530

    Adds responsive design in Content on mobile device

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import Title from "./Title"
 import List from "./List"
 
 function Content(){
-    return <div className="flex-col bg-blue-200 items-center justify-center mx-auto w-1/3">
+    return <div className="px-5 w-screen flex-col bg-blue-200 items-center justify-center xl:mx-auto xl:w-1/3 ">
         <Title />
         <List /> 
     </div>
