732238d496c7d16a3216f97351795c501a8bf94d
Kishan Kulkarni <98462020+Kishan-Kulkarni@users.noreply.github.com>
2023-02-19 20:08:00 +0530

    Add responsive design in Form on mobile device

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Form(props){
-    return <div className="pt-2 pl-1 mb-8">
-        <form >
-            <input type="text" name="listItem" placeholder="Please enter task" className="w-4/5 text-center h-10 rounded-full border-2" value={props.value} onChange={props.onChange} autoComplete="off"/>
-            <button type="submit" className="text-centre mx-2 bg-purple-500 text-white w-1/6 h-10 rounded-full" onClick={props.onClick}>Add</button>
+    return <div className="pt-2 px-1 mb-8 xl:pl-1">
+        <form className="xl:flex xl:w-full">
+            <input type="text" name="listItem" placeholder="Please enter task" className="w-full mb-3 text-center h-10 rounded-full border-2 xl:w-4/5 xl:mb-0 xl:w-4/5" value={props.value} onChange={props.onChange} autoComplete="off"/>
+            <div className="flex flex-col items-center xl:block xl:w-1/6 xl:mx-1">
+            <button type="submit" className="w-1/3 text-centre mx-2 bg-purple-500 text-white  h-10 rounded-full xl:mx-0 xl:w-full" onClick={props.onClick}>Add</button>
+            </div>
         </form>
     </div>
 }
