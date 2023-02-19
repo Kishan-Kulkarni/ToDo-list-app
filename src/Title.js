a8463a67d87966653a7286c6efcfb3fb980042f7
Kishan Kulkarni <kulkarnikishan610@gmail.com>
2023-02-19 14:03:38 +0530

    Minor bug fixes

--- src/Title.js
+++ src/Title.js
@@ -1,5 +1,4 @@
 import React from "react";
-import menu from "./icons8-hamburger-menu-16.png"
 
 function Title(){
     return <div className="bg-purple-500 mt-10 mb-7 text-white text-center text-2xl py-2 flex font-bold rounded">
